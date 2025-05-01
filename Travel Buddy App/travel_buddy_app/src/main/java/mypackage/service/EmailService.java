964a0dc334a19b2f62b161967039b47ba870d7f4
Baraa Omar <70182854+B-Nr@users.noreply.github.com>
2025-05-01 21:03:40 +0300

    added Jakarta email dependency and implemented EmailService methods. I used temporary email and password placeholders; to be updated in future.

--- Travel Buddy App/travel_buddy_app/src/main/java/mypackage/service/EmailService.java
+++ Travel Buddy App/travel_buddy_app/src/main/java/mypackage/service/EmailService.java
@@ -1,21 +1,109 @@
 package mypackage.service;
+import java.util.Properties;
 
+import jakarta.mail.Authenticator;
+import jakarta.mail.Message;
+import jakarta.mail.PasswordAuthentication;
+import jakarta.mail.Session;
+import jakarta.mail.Transport;
+import jakarta.mail.internet.InternetAddress;
+import jakarta.mail.internet.MimeMessage;
 import mypackage.model.User;
 
+
 public class EmailService {
+
+    private static final String FROM_EMAIL = "travelBuddies@gmail.com"; //To be changed
+    private static final String FROM_NAME = "Travel Buddies Team";
+    private static final String PASSWORD = "password"; //To be changed
+
+
+    public static boolean sendPasswordResetEmail(User user, String token) 
+    {
+        String toEmail = user.getE_mail();
+        String name = user.getNameSurname();
+        String subject = "Reset Password Request";
+
+        String body = "Hi " + name + ",\n\n" + "We received a request to reset your password.\n" + "Use the following token to reset your password in the app:\n\n" + token + "\n\n" + "If you did not request this, you can ignore this email.\n\n" + "Thanks,\n" + "Travel Buddies";
+
+        try 
+        {
+            Properties props = new Properties();
+            props.put("mail.smtp.auth", "true");
+            props.put("mail.smtp.starttls.enable", "true");
+            props.put("mail.smtp.host", "smtp.gmail.com");
+            props.put("mail.smtp.port", "587");
+
+            Session session = Session.getInstance(props, new Authenticator() 
+            {
+                protected PasswordAuthentication getPasswordAuthentication() 
+                {
+                    return new PasswordAuthentication(FROM_EMAIL, PASSWORD);
+                }
+            });
+
+            Message message = new MimeMessage(session);
+            message.setFrom(new InternetAddress(FROM_EMAIL, FROM_NAME));
+            message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(toEmail));
+            message.setSubject(subject);
+            message.setText(body);
+
+            Transport.send(message);
+
+            return true;
+
+        } 
+        catch (Exception e) 
+        {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    public static boolean verifyEmailToken(User user, String token, String inputToken) 
+    {
+        return inputToken != null && inputToken.equals(token);
+    }
+
+    public static boolean sendWelcomeEmail(User user)
+    {
+        String toEmail = user.getE_mail();
+        String name = user.getNameSurname();
+        String subject = "Welcome to Travel Buddies!";
+        
+        String body = "Hi " + name + ",\n\n" + "Welcome to Travel Buddies! We're excited to have you on board.\n" + "Start exploring and connecting with travelers from around the world.\n\n" + "Safe travels,\n" + "The Travel Buddies Team";
     
-    public static boolean sendPasswordResetEmail(User user, String token) {
-        // Implementation here
-        return false;
-    }
-
-    public static boolean verifyEmailToken(String token) {
-        // Implementation here
-        return false;
-    }
-
-    public static boolean sendWelcomeEmail(User user) {
-        // Implementation here
-        return false;
+        try 
+        {
+            Properties props = new Properties();
+            props.put("mail.smtp.auth", "true");
+            props.put("mail.smtp.starttls.enable", "true");
+            props.put("mail.smtp.host", "smtp.gmail.com");
+            props.put("mail.smtp.port", "587");
+    
+            Session session = Session.getInstance(props, new Authenticator() 
+            {
+                protected PasswordAuthentication getPasswordAuthentication() 
+                {
+                    return new PasswordAuthentication(FROM_EMAIL, PASSWORD);
+                }
+            });
+    
+            Message message = new MimeMessage(session);
+            message.setFrom(new InternetAddress(FROM_EMAIL, FROM_NAME));
+            message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(toEmail));
+            message.setSubject(subject);
+            message.setText(body);
+    
+            Transport.send(message);
+    
+            return true;
+    
+        } 
+        catch (Exception e) 
+        {
+            e.printStackTrace();
+            return false;
+        }
     }
 }
