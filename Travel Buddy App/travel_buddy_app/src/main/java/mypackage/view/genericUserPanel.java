8d8d0c61402de6f2cb4e1e9cf61d68948dd9b731
handekoksal <koksalhande1@gmail.com>
2025-05-11 07:34:05 +0300

    Create genericUserPanel.java
    new code

---
+++ Travel Buddy App/travel_buddy_app/src/main/java/mypackage/view/genericUserPanel.java
@@ -0,0 +1,33 @@
+package mypackage.view;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class genericUserPanels extends JPanel{
+    
+    static Color blueBack = new Color(204,228,252);
+    static Color blueFront = new Color(180,204,244);
+    //variables to keep track of the users entries' feautures
+    //date, city name, pp, etc.
+    //JournalEntry object
+    //TODO
+
+    public genericUserPanels() {
+        this.setBackground(blueBack);
+        //this.setSize(new Dimension(200,200));
+        this.setLayout(null);
+
+        //TODO Profile Picture Button
+
+        //Name Panel
+        JPanel namePanel = new JPanel();
+        namePanel.setBackground(blueFront);
+        namePanel.setSize(300,300);
+        namePanel.setBounds(20,120,260,150);
+        this.add(namePanel);
+        this.setVisible(true);
+
+    }
+}
