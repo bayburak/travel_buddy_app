fa0a1c293be12f42e75f7e6dc6e1fc5796240b7b
Setenay Yıldırım <setenayyildirim2006@gmail.com>
2025-05-12 22:57:09 +0300

    Update editProfileController.java

--- Travel Buddy App/travel_buddy_app/src/main/java/mypackage/controller/editProfileController.java
+++ Travel Buddy App/travel_buddy_app/src/main/java/mypackage/controller/editProfileController.java
@@ -3,6 +3,8 @@ package mypackage.controller;
 import javax.swing.*;
 import mypackage.model.User;
 import mypackage.view.editProfilePanel;
+import mypackage.view.login;
+import mypackage.view.signup;
 
 public class editProfileController
 {
@@ -33,6 +35,31 @@ public class editProfileController
             editPanel.getSaveButton().addActionListener(e -> {
                 editFrame.dispose();
                 profileFrame.setVisible(true);
+            });
+
+            editPanel.getDeleteAccountButton().addActionListener( e -> {
+                Object[] options = {"Yes", "No"};
+                int option = JOptionPane.showOptionDialog(
+                    null,
+                    "Do you want to delete your account?",
+                    "",
+                    JOptionPane.YES_NO_OPTION,
+                    JOptionPane.QUESTION_MESSAGE,
+                    null,
+                    options,
+                    options[1]
+                );
+                if (option == JOptionPane.YES_OPTION)
+                {
+                    user.deleteUser();
+                    editFrame.dispose();
+                    login loginView = new login();
+                    signup signupView = new signup();
+                    signupView.setVisible(false);
+                    loginView.setVisible(true);
+                    new AuthController(loginView, signupView);
+
+                }
             });
 
             editFrame.setExtendedState(JFrame.MAXIMIZED_BOTH);
